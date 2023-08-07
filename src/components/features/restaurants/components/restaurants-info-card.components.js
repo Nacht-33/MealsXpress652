0874effe182f395136bcfeba1a37fa9bd365206a
Nacht-33 <singhadhiman652@gmail.com>
2023-08-07 15:56:49 +0530

    Added Open and closed components

--- src/components/features/restaurants/components/restaurants-info-card.components.js
+++ src/components/features/restaurants/components/restaurants-info-card.components.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 import { SvgXml } from "react-native-svg";
 import star from "../../../../../assets/star";
-
+import open from "../../../../../assets/open";
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.ui.tertiary};
 `;
@@ -17,10 +17,29 @@ const Title = styled(Text)`
   font-size: ${(props) => props.theme.fontSizes.title};
   color: ${(props) => props.theme.colors.ui.primary};
 `;
+const Section = styled(View)`
+  padding: ${(props) => props.theme.space[2]} 0;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+const StatusSection = styled(View)`
+  flex-direction: row;
+`;
 const Rating = styled(View)`
   flex-direction: row;
-  padding: ${(props) => props.theme.space[2]} 0;
 `;
+
+const Closed = styled(Text)`
+  color: ${(props) => props.theme.colors.text.error};
+`;
+const ClosedSpacer = styled(View)`
+  padding-right: ${(props) => props.theme.space[3]};
+`;
+
+const ImageSpacer = styled(View)`
+  padding-left: ${(props) => props.theme.space[3]};
+`;
+
 const Address = styled(Text)`
   font-family: ${(props) => props.theme.fonts.body};
   font-size: ${(props) => props.theme.fontSizes.caption};
@@ -29,14 +48,14 @@ const Address = styled(Text)`
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Grace Foods",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "100 some random street",
     isOpenNow = true,
     rating = 3.5,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
   const ratingArray = Array.from(new Array(Math.floor(rating)));
   return (
@@ -50,11 +69,20 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
         <Info>
           <Title>{name}</Title>
 
-          <Rating>
-            {ratingArray.map(() => {
-              return <SvgXml xml={star} height={20} width={20}></SvgXml>;
-            })}
-          </Rating>
+          <Section>
+            <Rating>
+              {ratingArray.map(() => {
+                return <SvgXml xml={star} height={20} width={20}></SvgXml>;
+              })}
+            </Rating>
+            <StatusSection>
+              {isClosedTemporarily && <Closed>Closed Temporarily</Closed>}
+              <ClosedSpacer />
+              {isOpenNow && <SvgXml xml={open} height={20} width={20}></SvgXml>}
+              <ImageSpacer />
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </StatusSection>
+          </Section>
 
           <Address>{address}</Address>
         </Info>
